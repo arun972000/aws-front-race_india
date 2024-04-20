364e5f470d8fe959a4a2f0b60a07dba7410564a7
Arun pandian <arunpandian972000@gmail.com>
2024-04-20 17:09:11 +0530

    V4

--- src/components/Home/HomeBannerv4/HomeBannerV4.jsx
+++ src/components/Home/HomeBannerv4/HomeBannerV4.jsx
@@ -1,28 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+import "./HomeBannerv4.css";
 
-import { Row, Col } from 'react-bootstrap';
-import "./HomeBannerV4.css";
-
-import SliderImageV4 from './Sliderv4';
-
-
-
+import SliderImageV4 from "./Sliderv4";
 
 const HomeBannerV4 = () => {
-
-   
- 
-
-    return (
-
-        <Row className="g-2 mb-4">
-
-            <Col sm={12}>
-                <SliderImageV4 />
-            </Col>
-
-        </Row>
-
-    );
+  return (
+    <Row className="g-2 mb-4">
+      <Col sm={12}>
+        <SliderImageV4 />
+      </Col>
+    </Row>
+  );
 };
 
 export default HomeBannerV4;
